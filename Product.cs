39893a2e05259ec5c50c802efb26953926b693ef
Melody Miller <8bitmel0dee@gmail.com>
2023-10-30 02:31:38 -0400

    uploading ClassExercise1

---
+++ Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExercise1
+{
+    public class Product
+    {
+        // Properties of the product
+
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string Description { get; set; }
+
+    }
+
+    public class CatFood : Product
+    {
+        // Properties of cat food products
+        public double WeightPounds { get; set; }
+        public bool KittenFood { get; set; }
+
+    }
+
+    public class DogLeash : Product
+    {
+        public int LengthInches { get; set; }
+        public string Material { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using ClassExercise1;
+using System.Text.Json;
+
+
+
+Console.WriteLine("Press 1 to add a cat food product");
+Console.WriteLine("Type 'exit' to quit");
+
+string userInput = Console.ReadLine();
+
+CatFood catFood = new CatFood();
+
+
+
+while (userInput.ToLower() != "exit")
+{
+    if (userInput == "1")
+
+        Console.Write("Enter the brand name: ");
+
+    string name = Console.ReadLine();
+    catFood.Name = name;
+
+    Console.Write("Enter the price: ");
+
+    decimal price = decimal.Parse(Console.ReadLine());
+    catFood.Price = price;
+
+    Console.Write("Enter the quantity: ");
+
+    int quantity = int.Parse(Console.ReadLine());
+    catFood.Quantity = quantity;
+
+    Console.Write("Enter the description: ");
+
+    string description = Console.ReadLine();
+    catFood.Description = description;
+
+    Console.Write("Enter the weight in pounds: ");
+
+    double weight = double.Parse(Console.ReadLine());
+    catFood.WeightPounds = weight;
+
+    Console.Write("Is it kitten food? (yes/no): ");
+
+    string kitten = Console.ReadLine();
+    bool isKitten = kitten == "yes";
+    catFood.KittenFood = isKitten;
+
+    Console.WriteLine();
+
+
+    Console.WriteLine(JsonSerializer.Serialize(catFood));
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
